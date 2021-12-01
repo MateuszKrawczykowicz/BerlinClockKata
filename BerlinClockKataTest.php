1f7c423b7e3d2652caed7b551cb28ca95105c1bd
Mateusz Krawczykowicz <mateusz.krawczykowicz@student.vinci.be>
2021-12-01 20:11:07 +0100

    test clock2 01:00:00

--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -46,11 +46,16 @@ class BerlinClockKataTest extends TestCase
     }
 
     // function Clock
-    public function testClock(){
+    public function testClock1(){
         $firstLight = new BerlinClockKata();
         $actual = $firstLight->clock("22:53:05");
         $this->assertEquals("O\nY Y Y Y \nY Y O O \nY Y Y Y Y Y Y Y Y Y O \nY Y Y O ", $actual);
     }
 
+    public function testClock2(){
+        $firstLight = new BerlinClockKata();
+        $actual = $firstLight->clock("01:00:00");
+        $this->assertEquals("Y\nO O O O \nY O O O \nO O O O O O O O O O O \nO O O O ", $actual);
+    }
 
 }
