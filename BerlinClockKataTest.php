bd5fcfe17407372fba0c4ed46fa34389daff26e4
Jérôme Devaux <jerome.devaux@student.vinci.be>
2021-12-02 14:47:18 +0100

    Test9 pour les blocs de min

--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -64,6 +64,12 @@ class BerlinClockKataTest extends TestCase
         $this->assertEquals("Y Y Y Y Y Y Y O O O O ", $actual);
     }
 
+    public function testBlockMinShouldReturnEightY(){
+        $firstLight = new BerlinClockKata();
+        $actual = $firstLight->blockMin("11:40:22");
+        $this->assertEquals("Y Y Y Y Y Y Y Y O O O ", $actual);
+    }
+
     public function testBlockMinShouldReturnTenY(){
         $firstLight = new BerlinClockKata();
         $actual = $firstLight->blockMin("11:53:05");
